9516774ea6a1e00ba8852fc464e0b70990752bac
= <=>
2023-09-14 13:16:36 +0300

    test mainpy

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import pygame
 import random
 
-canvas = pygame.display.set_mode((640, 400))
+canvas = pygame.display.set_mode((1000, 1000))
 pygame.display.set_caption("Vihollispeli")
 
 def drawImage(file, x, y):
@@ -10,18 +10,19 @@ def drawImage(file, x, y):
 
 def drawing(canvas, characters):
     for character in characters:
-        kuvatiedosto, x, y, piirretaanko = character
-        if piirretaanko:
-            drawImage(kuvatiedosto, x, y)
+        file, x, y, isDrawn = character
+        if isDrawn:
+            drawImage(file, x, y)
 
 def control(characters, event):
     if event.type == pygame.KEYDOWN:
         if event.key == pygame.K_SPACE:
-            characters[0][3] = True
+            for character in characters:
+                character[3] = True
 
 def main():
-    characters = [["catSmall.png", 100, 100, True]]
-    
+    characters = [["catSmall.png", 100, 100, True], ["catSmallGreen.png", 300, 300, False]]
+    print(characters)
     while True:
         event = pygame.event.poll()
         if event.type == pygame.QUIT:
